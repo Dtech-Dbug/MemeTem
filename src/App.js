caff3899b892164149bcd3bda50d549ad9f3bce8
DC <dc@knownuggets.com>
2024-10-05 00:22:23 +0530

    code refactor

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import CollectionsPage from './Pages/CollectionsPage';
+import CollectionsPage from './Pages/TemplateCollections/index';
 import HomePage from './Pages/HomePage';
 import SplashScreen from "./Components/SplashScreen";
 import Navbar from './Components/navs/navbar'
