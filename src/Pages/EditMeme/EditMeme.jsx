7ffd35483599841e9afcd9be56d59509d1c8a05e
dtech-dbug <heydwaipayanhere@gmail.com>
2025-05-01 22:08:06 +0530

    fix: ux changes in edit panel

--- src/Pages/EditMeme/EditMeme.jsx
+++ src/Pages/EditMeme/EditMeme.jsx
@@ -13,17 +13,23 @@ const EditMeme = () => {
             {/* Edit Options */}
             <div className="w-80 p-4 bg-white shadow-md border rounded-lg flex flex-col gap-4">
                 <h2 className="text-lg font-bold">Edit Options</h2>
+                <hr />
                 <button onClick={handleAddText} className="px-4 py-2 text-white rounded primary-color">
                     + Add Text
                 </button>
-                <button onClick={handleDownload} className="px-4 py-2 text-white rounded secondary-color">
-                    Download
-                </button>
+                <hr/>
+                {/* font changes */}
                 <label className="text-sm font-medium custom-label-container">
                     Change Text Color:
                     <input type="color" className="custom-color-picker" onChange={handleColorChange} />
                 </label>
                 {/*  more controls like font size, position, etc. */}
+
+                {/* footer for dload button  */}
+                <button onClick={handleDownload} className="px-4 py-2 text-white rounded secondary-color mt-auto">
+                    Download
+                </button>
+            
             </div>
         </div>
     );
